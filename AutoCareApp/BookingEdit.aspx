27140aece0d211502c48063ab27d187ec1e468bb
Davies2101 <61430073+Davies2101@users.noreply.github.com>
2020-04-09 23:36:05 +0100

    Booking edit form and admin booking list view (in progress)
    Booking edit form and admin booking list view (in progress)

---
+++ AutoCareApp/BookingEdit.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BookingEdit.aspx.cs" Inherits="AutoCareApp.BookingEdit" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ AutoCareApp/ManageBookings.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AutoCareApp
+{
+    public partial class ManageBookings : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCancel_OnClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnStart_OnClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void btnEnd_OnClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
